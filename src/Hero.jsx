7a72eeae832ecff8afb15bcdf9356ac5953c0c3a
LeeWhoCodes <leewhocodes@gmail.com>
2024-11-09 19:03:17 +1300

    image added not correct size yet

---
+++ src/Hero.jsx
@@ -0,0 +1,10 @@
+import profilePic from './assets/LeeHeadshot.jpg'
+import './Hero.css'
+
+function Hero() {
+    return (
+            <img src={profilePic} alt="" className="profilePic" />
+    );
+}
+
+export default Hero
